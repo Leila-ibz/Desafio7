8fa33c96014476bbef0e3e4cd5d227bd02483cd7
adriansenturion <135929097+adriansenturion@users.noreply.github.com>
2023-06-09 20:05:47 -0300

    soy Adrián estoy probando subir
    solo estoy probando subir desde mi pc y Desktop

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,2 @@
 -- Active: 1686189676622@@127.0.0.1@3306@desafio7
-INSERT INTO usuario (nombre, apellido, telefono, username, email, contrasenia, estado, fecha_creacion, avatar, es_publico, es_colaborador, es_admin)
-VALUES
-    ('Leila', 'Ibañez', 3704682165, 'Leilaibz', 'leilaibanez@dominio.com', '123456', 'activo', '2023-06-09', DEFAULT, FALSE, FALSE, TRUE);
-
+-- Estoy subir. Soy Adrián.
