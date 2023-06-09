12c5c04ba84d121697a1af4a8da84cb838f082eb
Leila Ibañez <133930984+Leila-ibz@users.noreply.github.com>
2023-06-09 20:23:48 -0300

    Probamos subir un rol
    - Agregar el comando necesario que introduzca en la tabla usuario, 1 usuario con rol de admin(leila), 4 con rol de colaborador(adrian) y 5 con rol de público(emmanuel). Donde los campos: es_publico, es_colaborador y es_admin son booleanos

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,3 @@
--- Active: 1686189676622@@127.0.0.1@3306@desafio7
 INSERT INTO usuario (nombre, apellido, telefono, username, email, contrasenia, estado, fecha_creacion, avatar, es_publico, es_colaborador, es_admin)
 VALUES
-    ('Leila', 'Ibañez', 3704682165, 'Leilaibz', 'leilaibanez@dominio.com', '123456', 'activo', '2023-06-09', DEFAULT, FALSE, FALSE, TRUE);
-
+    ('Adrian', 'Senturion', 1234567890, 'adrians', 'adrian@dominio.com', '440066', 'activo', '2023-06-09', DEFAULT, FALSE, TRUE, FALSE);
